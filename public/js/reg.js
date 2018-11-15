f688d422340b4b4e1b1ca398a23e924eaf9a3dda
Dodder.B <408276492@qq.com>
2018-11-16 02:31:33 +0800

    reg&&login

--- public/js/reg.js
+++ public/js/reg.js
@@ -1,10 +1,14 @@
-// window.onload=function(){
-//     $uname = $("#MOBILE_UNAME");
-//     $unameLabel = $uname.parent().find("label");
-//     $uname.on("focus",function(){
-//         $unameLabel.addClass("noblock");
-//     });
-//     $uname.on("blur",function(){
-//         $unameLabel.removeClass("noblock");
-//     })
-// }
+window.onload = function(){
+    var $reg_suc_time =$("#COUNTDOWN");
+    var end = parseInt($reg_suc_time.html());
+    function downTask(){
+        if(end>0){
+            end--;
+            $reg_suc_time.html(end);
+        }else{
+            clearInterval(downTimer);
+            $reg_suc_time.html("0");
+        }
+    }
+    var downTimer=setInterval(downTask,1000);
+}
